f657b93daf3b78ecd075fead28f3c278733f5055
kiana-S <kiana.a.sheibani@gmail.com>
2023-01-07 23:25:12 -0500

    Fix package name

--- mobile/config.nix
+++ mobile/config.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 {
   hardware.bluetooth.enable = true;
-  hardware.pulseaudio.package = pkgs.pulseAudioFull;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
   zramSwap.enable = true;
 
   services.xserver.windowManager.sxmo.enable = true;
