e105184ce6b843d6811e5c4056d627474774b4cf
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-01-14 19:25:31 -0500

    Update mobile hardware configuration

--- hardware-configuration/mobile.nix
+++ hardware-configuration/mobile.nix
@@ -4,7 +4,7 @@
 { config, lib, pkgs, ... }:
 {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0e34be6f-a042-433e-bcfc-9df254a571c3";
+    { device = "/dev/disk/by-uuid/bd2fe7f4-b0e8-40b3-8da3-68f56dd49fab";
       fsType = "ext4";
     };
 
