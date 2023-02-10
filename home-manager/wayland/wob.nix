de450dbc1ac8dbe9aedb3d0140352eacd397825b
kiana-S <kiana.a.sheibani@gmail.com>
2023-02-10 09:54:53 -0500

    Fix Wob configuration

--- home-manager/wayland/wob.nix
+++ home-manager/wayland/wob.nix
@@ -6,8 +6,8 @@
     "wob/volume.ini".text = "";
 
     "wob/brightness.ini".text = config.xdg.configFile."wob/volume.ini".text + ''
-      border_color = #FFFF00FF
-      bar_color = #FFFF00FF
+      border_color = FFFF00FF
+      bar_color = FFFF00FF
     '';
   };
 }
