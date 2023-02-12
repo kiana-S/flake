da3833dbaacfd150e62f9295461693acbd567e81
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-02-11 19:57:20 -0500

    Turn on os-prober for desktop

--- hardware-configuration/desktop.nix
+++ hardware-configuration/desktop.nix
@@ -3,6 +3,7 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 {
+  boot.loader.grub.useOSProber = true;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
