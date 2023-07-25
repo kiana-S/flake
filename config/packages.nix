920c2802b82c0585a47d88a5ae233cbaab48c04a
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-07-25 00:34:26 -0400

    Use nixpkgs's exposed emacs29 derivation
    They finally added emacs 29 support to nixpkgs, so I don't have to do
    this weird hack anymore...

--- config/packages.nix
+++ config/packages.nix
@@ -3,25 +3,11 @@ let
   # nix-direnv with flake support
   nix-direnv-with-flakes = pkgs.nix-direnv.override { enableFlakes = true; };
 
-  emacs29 = pkgs.callPackage (import "${nixpkgs}/pkgs/applications/editors/emacs/generic.nix" {
-    version = "29.0.90";
-    sha256 = "sha256-5aR+9EZF9Md2nb4n3xktFR5j8cZto7mZaYUXZpQbvNI=";
-  }) {
+  emacs29 = pkgs.emacs29.override {
       withPgtk = true;
       withWebP = true;
       withSQLite3 = true;
-
-      # Copied from nixpkgs
-      libXaw = pkgs.xorg.libXaw;
-      gconf = null;
-      alsa-lib = null;
-      acl = null;
-      gpm = null;
-      inherit (pkgs.darwin.apple_sdk.frameworks)
-        AppKit Carbon Cocoa IOKit OSAKit Quartz QuartzCore WebKit
-        ImageCaptureCore GSS ImageIO;
-      inherit (pkgs.darwin) sigtool;
-    };
+  };
 in {
   environment.systemPackages = with pkgs; [
     ffmpeg
