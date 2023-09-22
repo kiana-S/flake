338a26c9425248d74048452a3181360ac64ae786
kiana-S <kiana.a.sheibani@gmail.com>
2023-09-21 23:43:27 -0400

    Enable power management tools

--- config/battery.nix
+++ config/battery.nix
@@ -6,5 +6,9 @@ lib.mkIf (config.platform == "laptop") {
     usePercentageForPolicy = true;
     percentageLow = 15;
     percentageCritical = 5;
-  };  
+  };
+
+  # Power and temperature management tools
+  services.thermald.enable = true;
+  services.tlp.enable = true;
 }
