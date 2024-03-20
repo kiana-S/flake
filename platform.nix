f940ad0218101be08107b040e14876fbef5eb9a6
kiana-S <kiana.a.sheibani@gmail.com>
2024-03-20 01:09:10 -0400

    Generalize platform option type
    There's no point in being restrictive here if we want to make it easier
    to add more platforms in the future.

--- platform.nix
+++ platform.nix
@@ -2,6 +2,6 @@
 {
   options.platform = lib.mkOption {
     description = "The platform to configure for";
-    type = lib.types.enum [ "desktop" "laptop" "mobile" ];
+    type = lib.types.str;
   };
 }
