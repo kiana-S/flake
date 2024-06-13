411e879de1614ce2b6afe7b67fcaf29337c861a4
kiana-S <kiana.a.sheibani@gmail.com>
2024-06-13 16:28:44 -0400

    Reorganize git config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, username, fullname, email, ... }:
+{ config, pkgs, username, ... }:
 {
   home.username = username;
   home.homeDirectory = "/home/" + username;
@@ -6,31 +6,4 @@
   home.stateVersion = "21.11";
 
   imports = [ ./shell ./wayland ./xdg.nix ./tools.nix ./email.nix ];
-
-  programs.git = {
-    enable = true;
-    userName = fullname;
-    userEmail = email;
-
-    signing.key = "6CB106C25E86A9F7";
-    signing.signByDefault = true;
-
-    extraConfig = {
-      credential.helper = "store";
-      git.allowForcePush = true;
-      push.followTags = true;
-      init.defaultBranch = "main";
-    };
-
-    delta.enable = true;
-    delta.options = {
-      features = "decorations";
-      relative-paths = true;
-
-      decorations = {
-        file-style = "yellow";
-        hunk-header-style = "line-number syntax";
-      };
-    };
-  };
 }
--- home-manager/tools.nix
+++ home-manager/tools.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, fullname, email, ... }:
 {
 
   # Password Store
@@ -17,5 +17,35 @@
 
   programs.mpv.config = {
     osc = false;
+  };
+
+  # Git
+
+  programs.git = {
+    enable = true;
+    userName = fullname;
+    userEmail = email;
+
+    signing.key = "6CB106C25E86A9F7";
+    signing.signByDefault = true;
+
+    extraConfig = {
+      github.user = "kiana-S";
+      credential.helper = "store";
+      git.allowForcePush = true;
+      push.followTags = true;
+      init.defaultBranch = "main";
+    };
+
+    delta.enable = true;
+    delta.options = {
+      features = "decorations";
+      relative-paths = true;
+
+      decorations = {
+        file-style = "yellow";
+        hunk-header-style = "line-number syntax";
+      };
+    };
   };
 }
